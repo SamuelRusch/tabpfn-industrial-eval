ab242e2ec4d5b8fcc775a5e5caa418f17faf3207
Kizzy <marcus@debera.de>
2026-05-28 21:00:15 +0200

    explode ergebnisse

--- data_split.py
+++ data_split.py
@@ -6,6 +6,10 @@ data_path = 'data/'
 
 df = pd.read_csv(data_path + 'Data_RSW.csv')
 df = df[df["Comments"] != "Communication error"]
+##############################################################
+#Comment out this line if you want to include the 'Explode' category in the dataset. 
+df = df[df["Category"] != "Explode"]
+##############################################################
 df = df.drop_duplicates(subset="Sample ID", keep="last")
 
 
